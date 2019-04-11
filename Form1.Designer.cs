5e09f4ab2af980e87d0327efbb111568634f4611
ShurumovMaxim <work6921@yandex.ru>
2019-04-11 18:59:54 +0700

    Add files via upload
    Main files of project

---
+++ Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_chains = new System.Windows.Forms.TextBox();
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            this.textBox_alphabet = new System.Windows.Forms.TextBox();
+            this.textBox_states = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_set_table = new System.Windows.Forms.Button();
+            this.textBox_fin_states = new System.Windows.Forms.TextBox();
+            this.textBox_start_state = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 179);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(326, 229);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.button1.Location = new System.Drawing.Point(427, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Проверить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_chains
+            // 
+            this.textBox_chains.Location = new System.Drawing.Point(427, 30);
+            this.textBox_chains.Multiline = true;
+            this.textBox_chains.Name = "textBox_chains";
+            this.textBox_chains.Size = new System.Drawing.Size(155, 20);
+            this.textBox_chains.TabIndex = 2;
+            // 
+            // textBox_result
+            // 
+            this.textBox_result.Location = new System.Drawing.Point(427, 95);
+            this.textBox_result.Multiline = true;
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.Size = new System.Drawing.Size(340, 313);
+            this.textBox_result.TabIndex = 3;
+            this.textBox_result.TextChanged += new System.EventHandler(this.textBox_result_TextChanged);
+            // 
+            // textBox_alphabet
+            // 
+            this.textBox_alphabet.Location = new System.Drawing.Point(12, 30);
+            this.textBox_alphabet.Name = "textBox_alphabet";
+            this.textBox_alphabet.Size = new System.Drawing.Size(128, 20);
+            this.textBox_alphabet.TabIndex = 4;
+            // 
+            // textBox_states
+            // 
+            this.textBox_states.Location = new System.Drawing.Point(186, 30);
+            this.textBox_states.Name = "textBox_states";
+            this.textBox_states.Size = new System.Drawing.Size(152, 20);
+            this.textBox_states.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Алфавит";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(183, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Состояния";
+            // 
+            // button_set_table
+            // 
+            this.button_set_table.Location = new System.Drawing.Point(12, 126);
+            this.button_set_table.Name = "button_set_table";
+            this.button_set_table.Size = new System.Drawing.Size(128, 36);
+            this.button_set_table.TabIndex = 8;
+            this.button_set_table.Text = "Создать таблицу";
+            this.button_set_table.UseVisualStyleBackColor = true;
+            this.button_set_table.Click += new System.EventHandler(this.button_set_table_Click);
+            // 
+            // textBox_fin_states
+            // 
+            this.textBox_fin_states.Location = new System.Drawing.Point(186, 80);
+            this.textBox_fin_states.Name = "textBox_fin_states";
+            this.textBox_fin_states.Size = new System.Drawing.Size(152, 20);
+            this.textBox_fin_states.TabIndex = 9;
+            // 
+            // textBox_start_state
+            // 
+            this.textBox_start_state.Location = new System.Drawing.Point(12, 80);
+            this.textBox_start_state.Name = "textBox_start_state";
+            this.textBox_start_state.Size = new System.Drawing.Size(128, 20);
+            this.textBox_start_state.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(183, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Конечные состояния";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Начальное состояние";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(186, 126);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 36);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Изменить состояния";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(427, 8);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Цепочка";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_start_state);
+            this.Controls.Add(this.textBox_fin_states);
+            this.Controls.Add(this.button_set_table);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_states);
+            this.Controls.Add(this.textBox_alphabet);
+            this.Controls.Add(this.textBox_result);
+            this.Controls.Add(this.textBox_chains);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_chains;
+        private System.Windows.Forms.TextBox textBox_result;
+        private System.Windows.Forms.TextBox textBox_alphabet;
+        private System.Windows.Forms.TextBox textBox_states;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_set_table;
+        private System.Windows.Forms.TextBox textBox_fin_states;
+        private System.Windows.Forms.TextBox textBox_start_state;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public string[] Alphabet;
+        public string[] States;
+        public string[] Mas_chains;
+        public string[] Fin_states;
+        public string [,] Matrix_trans;
+        public string depth = "";
+        public string start_state;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string[] Remove(string[] array, string item)
+        {
+            int remInd = -1;
+
+            for (int i = 0; i < array.Length; ++i)
+            {
+                if (array[i] == item)
+                {
+                    remInd = i;
+                    break;
+                }
+            }
+
+            string[] retVal = new string[array.Length - 1];
+
+            for (int i = 0, j = 0; i < retVal.Length; ++i, ++j)
+            {
+                if (j == remInd)
+                    ++j;
+
+                retVal[i] = array[j];
+            }
+
+            return retVal;
+        }
+        private void Form1_Load(object sender, EventArgs e)//заполняем автомат при запуске (цепочки с '00')
+        {
+            AllocConsole();/*
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            //установка столбцов (входы)
+            this.dataGridView1.Columns.Add("Column1", "0");
+            this.dataGridView1.Columns.Add("Column2", "1");
+            this.dataGridView1.Columns.Add("Column3", "2");
+            //this.dataGridView1.Columns.Add("Колонка 3", "Заголовок 3");
+            //заполнение строк (переходы)
+            this.dataGridView1.Rows.Add("B", "", "");
+            this.dataGridView1.Rows.Add("", "C", "");
+            this.dataGridView1.Rows.Add("D", "D", "D");
+            this.dataGridView1.Rows.Add("C", "C", "C");
+            //установка заголовков строк (состояния)
+            this.dataGridView1.Rows[0].HeaderCell.Value = "A";
+            this.dataGridView1.Rows[1].HeaderCell.Value = "B";
+            this.dataGridView1.Rows[2].HeaderCell.Value = "C";
+            this.dataGridView1.Rows[3].HeaderCell.Value = "D";
+            */
+        }
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Console.Write(States[0]);
+            //проверка на пустые состояния
+            if (this.textBox_start_state.Text == "" || this.textBox_fin_states.Text == "")
+            {
+                MessageBox.Show("Отсутствует стартовое или конечные состояния");
+                return;
+            }
+            int count_real_states = 0;
+            for (int i = 0; i < this.dataGridView1.RowCount - 1; i++)
+                if (this.dataGridView1.Rows[i].HeaderCell.Value != null)
+                    count_real_states++;
+            //заполняю массивы состояний и алфавита
+            Alphabet = new string[this.dataGridView1.ColumnCount];
+            States = new string[count_real_states];
+            
+            for (int i = 0; i < this.dataGridView1.ColumnCount; i++)
+                Alphabet[i] = this.dataGridView1.Columns[i].HeaderText;
+
+            for (int i = 0; i < count_real_states; i++)
+                States[i] = this.dataGridView1.Rows[i].HeaderCell.Value.ToString();
+                
+           
+            //Console.Write(States.Length.ToString());
+
+
+            //Console.Write(States[i]);
+            //Console.Write(Alphabet[0] + Alphabet[1]);
+            //Console.Write(States.Length);
+            //запоняем матрицу переходов
+           // Console.Write("\n");
+            Matrix_trans = new string[States.Length, Alphabet.Length];
+            for (int i = 0; i < States.Length; i++)
+                for (int j = 0; j < Alphabet.Length; j++)
+                    try { Matrix_trans[i, j] = this.dataGridView1.Rows[i].Cells[j].Value.ToString(); }
+                    catch { Matrix_trans[i, j] = ""; }
+
+
+            for (int i = 0; i < States.Length; i++)
+            {
+                for (int j = 0; j < Alphabet.Length; j++)
+                //Matrix_trans[i, j] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                {
+                    Console.Write(Matrix_trans[i, j] + " ");
+                }
+                Console.Write("\n");
+            }
+            //формируем массив цепочек
+            Mas_chains = this.textBox_chains.Text.Split(',');
+            Init_some();
+            //паихали
+            int start = Get_state(start_state);
+            for (int i = 0; i < Mas_chains.Length; i++)
+            {
+                if(!Check_chain_for_alphabet(Mas_chains[i]))
+                {
+                    this.textBox_result.Text += Mas_chains[i] + " — в данной цепочке присутствуют посторонние символы" + Environment.NewLine;
+                    continue;
+                }
+                Vivod(0, start, Mas_chains[i]);
+                this.textBox_result.Text +=  Environment.NewLine;
+                depth = "";
+
+            }
+             //   for(int j = 0; j < )
+        }
+        private void Vivod(int cur_symbol_ind, int cur_state, string Chain)
+        {
+            bool flag_exit = true;
+           // int next_state = cur_state;
+            this.textBox_result.Text += " Вывод для " + Chain  + " :" + Environment.NewLine;
+            if (Chain == "")
+            {
+                if (Fin_states.Contains(start_state))
+                {
+                    this.textBox_result.Text += Chain + "Пустая цепочка принадлежит, т.к. начальное состояние совпадает с конечными " + Environment.NewLine;
+                    return;
+                }
+                else {
+                    this.textBox_result.Text += Chain + "Пустая цепочка не принадлежит, т.к. начальное состояние не совпадает с конечными" + Environment.NewLine;
+                    return;
+                }
+                
+                
+            }
+            
+            //this.textBox_result.Text += depth + "<" + Matrix_trans[cur_state, i] + "," + Get_chain_at_ind(Chain, cur_symbol_ind) + ">" + Environment.NewLine;
+            //depth += "-";
+            depth += "-";
+            while (cur_symbol_ind != Chain.Length)
+            {
+                flag_exit = true;
+                //cur_state = next_state;
+                for (int i = 0; i < Alphabet.Length; i++)
+                {
+                    
+                    if (Chain[cur_symbol_ind].ToString() == Alphabet[i])
+                    {
+                        flag_exit = false;
+                        this.textBox_result.Text +=  "Состояние - " + this.dataGridView1.Rows[cur_state].HeaderCell.Value + ". Оставшаяся цепочка - "+ Get_chain_at_ind(Chain, cur_symbol_ind) + Environment.NewLine;
+                        //depth += "-";
+                        //if (cur_symbol_ind != Chain.Length - 1)
+                        if(Matrix_trans[cur_state, i] != "")
+                            cur_state = Get_state(Matrix_trans[cur_state, i]);
+                        else
+                        {
+                            this.textBox_result.Text += Chain +  " - данная цепочка не принадлежит, т.к. нет перехода из состояния " + States[cur_state] + "' по символу '" + Alphabet[i].ToString()+ "'" + Environment.NewLine;
+                            return;
+                        }
+                        //next_state = Get_state(Matrix_trans[cur_state, i]);
+                        cur_symbol_ind++;
+                        break;
+                    }
+                }
+                if (flag_exit)// не нашли переход
+                {
+                    this.textBox_result.Text += Chain +" - данная цепочка не принадлежит, т.к. не найден переход" + Environment.NewLine;
+                    return;
+                }
+            }
+            if (Fin_states.Contains(States[cur_state]))
+            {
+                //this.textBox_result.Text += depth + "<" + this.dataGridView1.Rows[cur_state].HeaderCell.Value + "," + Get_chain_at_ind(Chain, cur_symbol_ind) + ">" + Environment.NewLine;
+                this.textBox_result.Text += Chain + " - данная цепочка принадлежит, т.к. достигнуто конечное состояние " + this.dataGridView1.Rows[cur_state].HeaderCell.Value + Environment.NewLine;
+                return;
+            }
+            else
+            {
+                //this.textBox_result.Text += depth + "<" + this.dataGridView1.Rows[cur_state].HeaderCell.Value + "," + Get_chain_at_ind(Chain, cur_symbol_ind) + ">" + Environment.NewLine;
+
+                this.textBox_result.Text += Chain + " - данная цепочка не принадлежит, т.к. она дочитана, но состояние " + this.dataGridView1.Rows[cur_state].HeaderCell.Value + " не конечное" + Environment.NewLine;
+                return;
+            }
+            
+        }
+        private string Get_chain_at_ind(string chain, int ind)
+        {
+            string str = "";
+            for (int i = 0; i < chain.Length; i++)
+                if (i >= ind)
+                    str += chain[i].ToString();
+            return str;
+        }
+
+        private int Get_state(string str)
+        {
+            for (int i = 0; i < States.Length; i++)
+            {
+                if (str == States[i])
+                    return i;
+            }
+            MessageBox.Show("Get_next_state" + str);
+            return -1;
+        }
+
+        private void Init_some()
+        {
+            if (this.textBox_start_state.Text == "" || this.textBox_fin_states.Text == "")
+            {
+                MessageBox.Show("Отсутствуют стартовое или конечные состояния");
+                return;
+            }
+            Fin_states = this.textBox_fin_states.Text.Split(',');
+            Fin_states = Fin_states.Distinct().ToArray();
+            start_state = this.textBox_start_state.Text;
+            
+            foreach (string i in Fin_states)//проверка на совпадения конечных состояний и состояний
+            {
+                if (!States.Contains(i))
+                {
+                    MessageBox.Show("Конечное состояние " + i + " отсутствует в обычных состояниях");
+                    return;
+                }
+            }
+            if (!States.Contains(start_state))
+            {
+                MessageBox.Show("Начальное состояние отсутствует в обычных состояниях");
+                return;
+            }
+        }
+
+        private void button_set_table_Click(object sender, EventArgs e)
+        {
+           
+                
+            // проверки на ввод
+
+            Alphabet = this.textBox_alphabet.Text.Split(',');
+            States = this.textBox_states.Text.Split(',');
+            Init_some();
+            foreach (string i in Alphabet)//проверка на пустоту строк
+            {
+                if (i == "")
+                    Alphabet = Remove(Alphabet, i);
+            }
+            Alphabet = Alphabet.Distinct().ToArray();//удаление одинаковых
+            foreach (string i in States)//проверка на пустоту строк
+            {
+                if (i == "")
+                    States = Remove(States, i);
+            }
+            States = States.Distinct().ToArray();//удаление одинаковых
+            if (Alphabet.Length == 0)
+            {
+                MessageBox.Show("Задайте алфавит");
+                return;
+            }
+            foreach (string i in States)//проверка на совпадения в алфавита и состояний
+            {
+                if (Alphabet.Contains(i))
+                {
+                    MessageBox.Show("Элемент алфавита " + i + " сопадает с состоянием");
+                    return;
+                }
+            }
+            
+            //for (int i = 0; i < Alphabet.Length; i++)
+            //  MessageBox.Show(Alphabet[i]);
+            this.dataGridView1.Columns.Clear();
+            this.dataGridView1.Rows.Clear();
+            //строим таблицу с заданными столбцами и строками
+            for (int i = 0; i < Alphabet.Length; i++)
+            {
+                this.dataGridView1.Columns.Add("Column"+ i.ToString(), Alphabet[i]);      
+            }
+            for (int i = 0; i < States.Length; i++)
+            {
+                this.dataGridView1.Rows.Add();
+                this.dataGridView1.Rows[i].HeaderCell.Value = States[i]; 
+            }
+
+        }
+        private bool Check_chain_for_alphabet(string Chain)
+        {
+            foreach (char i in Chain)
+            {
+                if (Alphabet.Contains(i.ToString()) == false)
+                    return false;
+            }
+            return true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Init_some();
+        }
+
+        private void textBox_result_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
